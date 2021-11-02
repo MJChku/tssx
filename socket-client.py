e603968e859e45b29ed8e417c06d16ac2b911512
jiachengma <mjc97cooper@gmail.com>
2021-11-02 07:19:38 +0000

    add ython3 example

---
+++ socket-client.py
@@ -0,0 +1,23 @@
+
+import socket
+import sys 
+from time import time
+import numpy  as np
+n = 4
+# a = np.random.uniform(size=n)
+a = np.arange(0, n).astype(float)
+a_size = sys.getsizeof(a)
+abytes = a.tobytes()
+print(np.frombuffer(abytes).sum())
+print ('%d bytes, %dKB, %dMB' % (a_size, a_size/1024, a_size/1024/1024))
+
+with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
+    client.connect("./test")
+    c = 10000000
+    st = time()
+    while c:
+        client.send(abytes)
+        c -= 1
+        # print(c)
+    print("time ", time()-st)
+    client.close()
---
+++ socket-server.py
@@ -0,0 +1,29 @@
+
+import socket
+import sys 
+from time import time
+import os 
+os.unlink("./test")
+import numpy as np
+sumMean = 0
+a = 0
+
+with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as s:
+    s.bind("./test")
+    s.listen(1)
+    conn, addr = s.accept()
+    st = time()
+    # print("whatsup")
+    c = 100
+    while True:
+        data = conn.recv(4096)
+        nparr = np.frombuffer(data, dtype=float)
+        if nparr.size > 0:
+            sumMean += nparr.sum()
+        # a += sys.getsizeof(nparr)
+        # print(4*1000000-a)
+        if sumMean == 6*10000000:
+            print("time ", time()-st, sumMean)
+            s.close()
+            exit()
+   
